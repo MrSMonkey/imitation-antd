c2349faf714a0d3327d15894a176f96856c25dcf
李春霖 <chunlin.li7@mycos.com>
2021-06-04 17:12:34 +0800

    给组件添加storybook

---
+++ .storybook/preview.tsx
@@ -0,0 +1,32 @@
+
+import { configure, addDecorator, addParameters } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info'
+import React from 'react'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import "./../src/styles/index.scss"
+library.add(fas)
+
+const wrapperStyle: React.CSSProperties = {
+  padding: '20px 40px'
+}
+
+const storyWrapper = (stroyFn: any) => (
+  <div style={wrapperStyle}>
+    <h3>组件演示</h3>
+    {stroyFn()}
+  </div>
+)
+
+addDecorator(storyWrapper)
+addDecorator(withInfo)
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
